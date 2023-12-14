a52a00c1ad101b895091b4d3204260861a0a91ba
zlevin1228 <zacharylevin1228@gmail.com>
2023-12-14 14:56:09 -0500

    added java and changed some html

---
+++ script.js
@@ -0,0 +1,16 @@
+function shakeMagic8Ball() {
+    const question = prompt('Ask the Magic 8-Ball a question:');
+    
+    if (question === null){
+        document.getElementById('response-text').innerText = 'YOU WERE SUPPOSED TO ASK A QUESTION!!!!'
+        document.getElementById('response-text').classList = 'text-danger' //changes HTML classes
+        document.getElementById('response-image').src = '#BAD/BROKENIMAGE'
+    } else {
+        document.getElementById('response-text').innerText = 'Magic 8-Ball says:'
+    }
+
+    if (!question.trim()){
+        alert('Please enter a valid question! This must mean you did not drop it low!!')
+        return
+    } //.trim cuts off any spaces after the last physical character and leaves you with an empty string which is a falsy
+}
