ea4d266a5bd7518d9b677ec8a3b181cc17fcf742
zlevin1228 <zacharylevin1228@gmail.com>
2023-12-18 13:32:59 -0500

    website complete. zoltar can speak

--- script.js
+++ script.js
@@ -1,16 +1,70 @@
 function shakeMagic8Ball() {
-    const question = prompt('Ask the Magic 8-Ball a question:');
+    const question = prompt('Ask the Zoltar a question:');
     
     if (question === null){
-        document.getElementById('response-text').innerText = 'YOU WERE SUPPOSED TO ASK A QUESTION!!!!'
-        document.getElementById('response-text').classList = 'text-danger' //changes HTML classes
+        document.getElementById('response-text').innerText = 'YOU WERE SUPPOSED TO ASK A QUESTION!!!! ZOLTAR WANTS TO SPEAK!'
+        document.getElementById('response-text').classList = 'text-danger text-bg-dark' //changes HTML classes
         document.getElementById('response-image').src = '#BAD/BROKENIMG'
     } else {
-        document.getElementById('response-text').innerText = 'Magic 8-Ball says:'
+        document.getElementById('response-text').innerText = 'Zoltar speaks:'
     }
 
     if (!question.trim()){
-        alert('Please enter a valid question! This must mean you did not drop it low!!')
+        alert('Please enter a valid question! Zoltar would like to speak!')
         return
     } //.trim cuts off any spaces after the last physical character and leaves you with an empty string which is a falsy
+    
+    const randomNumber = Math.floor(Math.random() * 8);
+
+    let answer, image, color;
+
+    switch(randomNumber){
+        case 0:
+            answer = "Should result in a yes";
+            image = "Images/check.png" //change
+            color = "text-success text-bg-dark"
+            break;
+        case 1:
+            answer = "It is decidedly so";
+            image = "Images/check.png" //change
+            color = "text-success text-bg-dark"
+            break; 
+        case 2:
+            answer = "Reply hazy try again";
+            image = "Images/questionmark.png" //change
+            color = "text-secondary text-bg-light"
+            break; 
+        case 3:
+            answer = "Cannot predict now";
+            image = "Images/questionmark.png" //change
+            color = "text-secondary text-bg-light"
+            break; 
+        case 4:
+            answer = "Do not count on it";
+            image = "Images/x.png" //change
+            color = "text-danger text-bg-dark"
+            break; 
+        case 5:
+            answer = "My sources say no";
+            image = "Images/x.png" //change
+            color = "text-danger text-bg-dark"   
+            break; 
+        case 6:
+            answer = "Outlook not so good";
+            image = "Images/x.png" //change
+            color = "text-danger text-bg-dark"
+            break; 
+        case 7:
+            answer = "Positive result will occur";
+            image = "Images/check.png" //change
+            color = "text-success text-bg-light"
+            break; 
+    }
+
+    console.log(answer);    
+
+    const responseText = document.getElementById('response-text')
+    responseText.innerText = answer
+    responseText.classList = `display-4 ${color}`
+    document.getElementById('response-image').src = image
 }
